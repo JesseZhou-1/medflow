394f25338940e850cecccfdce109113bc009a55c
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2024-09-25 00:33:45 -0500

    Update trainmed.py

--- medsimGNF/trainmed.py
+++ medsimGNF/trainmed.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch  # Importing the PyTorch library, which provides tools for deep learning.
 import pickle  # Importing the pickle module, which allows to serialize and deserialize Python object structures.
 import ast
-from cGNF.training import train
+from cGNF import train
 import collections.abc
 collections.Iterable = collections.abc.Iterable
 import networkx as nx

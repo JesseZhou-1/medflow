ad9ec8618ff7120ac8e2ff52cf0bc286cfb787a6
Jesse Zhou <zhou.jesse2@gmail.com>
2025-06-13 20:24:35 -0500

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="medflow",
-    version="0.1.5",  # bump for each release
+    version="0.1.6",  # bump for each release
     author="Jesse Zhou",
     author_email="jessezhou1@uchicago.edu",
     description=(
